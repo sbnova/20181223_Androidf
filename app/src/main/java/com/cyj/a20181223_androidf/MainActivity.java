23de52ef38c62db3d37d6a2c0e85a29829e919cb
test <test1>
2018-12-23 12:40:38 +0900

    타임피커 다이얼로그 예시

--- app/src/main/java/com/cyj/a20181223_androidf/MainActivity.java
+++ app/src/main/java/com/cyj/a20181223_androidf/MainActivity.java
@@ -1,20 +1,26 @@
 package com.cyj.a20181223_androidf;
 
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
 
     private android.widget.Button logoutBtn;
+    private android.widget.TextView timeTxt;
+    private Button timePickerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         bindViews();
         setupEvent();
@@ -42,6 +48,23 @@ public class MainActivity extends BaseActivity {
                 alert.show();
             }
         });
+
+        timePickerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TimePickerDialog.OnTimeSetListener otsl = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String selectedTime = String.format("%d시 %d분", hourOfDay, minute);
+                        timeTxt.setText(selectedTime);
+                    }
+                };
+
+                TimePickerDialog tpd = new TimePickerDialog(mContext, otsl, 13, 30, false);
+                tpd.show();
+            }
+        });
     }
 
     @Override
@@ -52,5 +75,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public void bindViews() {
         this.logoutBtn = (Button) findViewById(R.id.logoutBtn);
+        this.timePickerBtn = (Button) findViewById(R.id.timePickerBtn);
+        this.timeTxt = (TextView) findViewById(R.id.timeTxt);
     }
 }
