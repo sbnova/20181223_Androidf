2198b3aa6b4f0842adbd4819528381824a24ca6a
test <test1>
2018-12-23 12:25:47 +0900

    AlertDialog 예시

--- app/src/main/java/com/cyj/a20181223_androidf/MainActivity.java
+++ app/src/main/java/com/cyj/a20181223_androidf/MainActivity.java
@@ -1,9 +1,15 @@
 package com.cyj.a20181223_androidf;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,24 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvent() {
 
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                경고창을 띄우고, 확인을 누르면 로그아웃 되었다는 토스트
+//                취소를 누르면 아무일도 없던것으로 처리.
+                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                alert.setTitle("로그아웃 확인");
+                alert.setMessage("정말 로그아웃 하시겠어요?");
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(mContext, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setNegativeButton("취소", null);
+                alert.show();
+            }
+        });
     }
 
     @Override
@@ -27,6 +51,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        this.logoutBtn = (Button) findViewById(R.id.logoutBtn);
     }
 }
