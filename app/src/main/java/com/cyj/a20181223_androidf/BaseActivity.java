fb1051ec115da8c34fd699f4e78bc83605d96a95
test <test1>
2018-12-23 12:16:24 +0900

    Initial commit

---
+++ app/src/main/java/com/cyj/a20181223_androidf/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.cyj.a20181223_androidf;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvent();
+    public abstract void setValues();
+    public abstract void bindViews();
+
+}
---
+++ app/src/main/java/com/cyj/a20181223_androidf/MainActivity.java
@@ -0,0 +1,32 @@
+package com.cyj.a20181223_androidf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvent();
+        setValues();
+    }
+
+    @Override
+    public void setupEvent() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
